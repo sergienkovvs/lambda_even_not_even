5c82b579854982be11e0c303d849b85040586442
vitalijsergienko <sergienkovvs98@gmail.com>
2022-08-28 21:58:05 +0300

    New lambda

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import random
 
 nums_ran = random.randint(0,400)
 
-var = (lambda nums_ran: nums_ran % 2)
-if var(nums_ran):
-    print("Не чётное")
-else:
-    print("Чётное")
-var(nums_ran)
-print(var(nums_ran))
+num = lambda: "Не чётное" if nums_ran % 2 else "Чётное"
+
+print(num(), nums_ran)
